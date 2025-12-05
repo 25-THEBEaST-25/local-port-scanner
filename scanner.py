1bfb6bc80b023d93d6aa14ad3c821d7dfa919617
Aryan Wesavkar <aryan25wes@gmail.com>
2025-12-05 12:51:53 +0530

    Improve scanner indentation and CLI usage

--- scanner.py
+++ scanner.py
@@ -1,7 +1,15 @@
+import sys
+
 import socket
 
 # Ask user for target IP or hostname
-target = input("Enter IP address or hostname to scan (e.g. 127.0.0.1): ")
+# If IP/host is given as a command-line argument, use that
+if len(sys.argv) > 1:
+    target = sys.argv[1]
+else:
+    # Otherwise ask the user
+    target = input("Enter IP address or hostname to scan (e.g. 127.0.0.1): ")
+
 
 # Common ports to scan
 ports = [21, 22, 23, 80, 443, 3000, 5000, 8080]
@@ -10,7 +18,7 @@ print(f"\nScanning target: {target}\n")
 
 for port in ports:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1)  # 1 second timeout
+    s.settimeout(1)
 
     try:
         result = s.connect_ex((target, port))
